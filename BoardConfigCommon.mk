e00724adaac59a9fcafc6378ca13376c54268215
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-10 13:36:52 +0000

    sm8150-common: Simplify AVB flag logic
    * AVB's `--flags 3` arguement builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,8 +196,7 @@ include $(COMMON_PATH)/sepolicy/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
 BOARD_AVB_VBMETA_SYSTEM := system product system_ext
